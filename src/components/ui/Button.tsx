4d47b0e8cf13a86a0ab238d335134e7a6eb9bf03
Dani <danial@Danis-MacBook-Pro.local>
2023-05-19 00:38:47 +0330

    add primary theme

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,19 +1,22 @@
 import React, { PropsWithChildren } from "react";
 
 const Button = ({
-    children,
-    ...props
+  children,
+  ...props
 }: PropsWithChildren<
-    React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+  React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  >
 >) => {
-    return (
-        <button
-            {...props}
-            className={`bg-btn text-center text-white p-2.5 gap-2.5 rounded-md flex justify-center items-center ${props.className}`}
-        >
-            {children}
-        </button>
-    );
+  return (
+    <button
+      {...props}
+      className={`bg-primary text-center text-white p-2.5 rounded-md ${props.className}`}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 import Button from "../components/ui/Button";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>صفحه لاگین</h1>
-            <Button>
-                <Link to="/register">Go To Register</Link>
-            </Button>
-        </div>
-    );
+  return (
+    <div className="flex flex-row">
+      <h1>صفحه لاگین</h1>
+      <Button>
+        <Link to="/register">انتقال به صفحه ساخت کاربر</Link>
+      </Button>
+    </div>
+  );
 };
 
 export default Login;

2442a8341a70c16a48aa2c9e0978e54597d0cc5f
SeyedAmirMohammad <41320511+SeyedAmirMohammad@users.noreply.github.com>
2023-05-18 19:14:20 +0330

    Button design done first Amirmuhammad commit

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,19 +1,19 @@
 import React, { PropsWithChildren } from "react";
 
 const Button = ({
-  children,
-  ...props
+    children,
+    ...props
 }: PropsWithChildren<
-  React.DetailedHTMLProps<
-    React.ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement
-  >
+    React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
 >) => {
-  return (
-    <button {...props} className="bg-slate-400 p-2 rounded-md">
-      {children}
-    </button>
-  );
+    return (
+        <button
+            {...props}
+            className={`bg-btn text-center text-white p-2.5 gap-2.5 rounded-md flex justify-center items-center ${props.className}`}
+        >
+            {children}
+        </button>
+    );
 };
 
 export default Button;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 import Button from "../components/ui/Button";
 
 const Login = () => {
-  return (
-    <div>
-      <h1>صفحه لاگین</h1>
-      <Button>
-        <Link to="/register">Go To Register</Link>
-      </Button>
-    </div>
-  );
+    return (
+        <div>
+            <h1>صفحه لاگین</h1>
+            <Button>
+                <Link to="/register">Go To Register</Link>
+            </Button>
+        </div>
+    );
 };
 
 export default Login;

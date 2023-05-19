46e37ed113f33544c0077d0a3b3b4c58349b610b
Dani <danial@Danis-MacBook-Pro.local>
2023-05-19 18:47:23 +0330

    start to add bg to layout

--- src/components/AnonymousHeader.tsx
+++ src/components/AnonymousHeader.tsx
@@ -3,7 +3,7 @@ import Button from "./ui/Button";
 
 function AnonymousHeader() {
   return (
-    <div className="px-8 flex flex-row justify-between sticky top-8">
+    <div className="p-8 flex flex-row justify-between">
       <span className="text-3xl font-bold bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent">
         کوئرا تسک منیجر
       </span>
--- src/layouts/AnonymousLayout.tsx
+++ src/layouts/AnonymousLayout.tsx
@@ -3,10 +3,13 @@ import AnonymousHeader from "../components/AnonymousHeader";
 
 const AnonymousLayout = () => {
   return (
-    <>
+    <div className="flex flex-col h-screen">
+      <div className="w-screen h-screen m-auto absolute -z-50">
+        <div className="absolute inset-0 bg-gradient-to-r from-secondary  to-primary transform -skew-y-12 origin-bottom-right"></div>
+      </div>
       <AnonymousHeader />
       <Outlet />
-    </>
+    </div>
   );
 };
 

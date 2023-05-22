b1e014dd2643cc966a7f41894338a1fe7f792bcb
SeyedAmirMohammad <41320511+SeyedAmirMohammad@users.noreply.github.com>
2023-05-22 22:52:44 +0330

    Card edit design done 5th Amirmuhammad commit on editCard branch

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,17 +1,34 @@
 import React, { PropsWithChildren } from "react";
 
-function Card({
-  children,
-  className,
-  ...props
-}: PropsWithChildren<
-  React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
->) {
+interface Props
+  extends PropsWithChildren<React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>> {
+  title?: string;
+  shadow?: boolean;
+  exit?: boolean;
+  back?: boolean;
+}
+
+function Card({ title, exit, back, shadow = true, children, className, ...props }: Props) {
   return (
     <div
       {...props}
-      className={`rounded-2xl bg-white shadow-[0px_12px_50px_rgba(0,0,0,0.18)] m-2 max-w-[520px] p-6 ${className}`}
+      className={`rounded-2xl bg-white ${
+        shadow && "shadow-[0px_12px_50px_rgba(0,0,0,0.18)]"
+      } m-2 max-w-[520px] p-6 ${className}`}
     >
+      {(title || exit || back) && (
+        <div>
+          {
+            <div className="grid grid-flow-col  ">
+              <div className="w-2 h-2 cursor-pointer text-gray">{exit ? "x" : ""}</div>
+              <div className="justify-self-center font-semibold text-base sm:text-xl md:text-2xl lg:text-3xl">
+                {title && <p>{title}</p>}
+              </div>
+              <div className="justify-items-end text-left cursor-pointer text-gray">{back ? "->" : ""}</div>
+            </div>
+          }
+        </div>
+      )}
       {children}
     </div>
   );

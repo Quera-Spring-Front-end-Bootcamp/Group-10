8471c87acb363701f83eafdd0f372c4dfb58fcdd
SeyedAmirMohammad <41320511+SeyedAmirMohammad@users.noreply.github.com>
2023-05-25 17:32:52 +0330

    Edit color Palette done 13th commit Amirmuhammad on FileInput commit

--- src/components/ui/ColorPalette.tsx
+++ src/components/ui/ColorPalette.tsx
@@ -13,16 +13,16 @@ interface Color {
 }
 
 const colors: Color[] = [
-  { id: 1, colorHEX: "#208D8E", active: true },
-  { id: 2, colorHEX: "#78C6B0", active: false },
-  { id: 3, colorHEX: "#76BC86", active: false },
-  { id: 4, colorHEX: "#80DC69", active: false },
-  { id: 5, colorHEX: "#E46161", active: false },
-  { id: 6, colorHEX: "#E17E80", active: false },
-  { id: 7, colorHEX: "#EC8182", active: false },
-  { id: 8, colorHEX: "#F3C567", active: false },
-  { id: 9, colorHEX: "#E57A57", active: false },
-  { id: 10, colorHEX: "#F1A25C", active: false },
+  { id: 1, colorHEX: "bg-[#208D8E]", active: true },
+  { id: 2, colorHEX: "bg-[#78C6B0]", active: false },
+  { id: 3, colorHEX: "bg-[#76BC86]", active: false },
+  { id: 4, colorHEX: "bg-[#80DC69]", active: false },
+  { id: 5, colorHEX: "bg-[#E46161]", active: false },
+  { id: 6, colorHEX: "bg-[#E17E80]", active: false },
+  { id: 7, colorHEX: "bg-[#EC8182]", active: false },
+  { id: 8, colorHEX: "bg-[#F3C567]", active: false },
+  { id: 9, colorHEX: "bg-[#E57A57]", active: false },
+  { id: 10, colorHEX: "bg-[#F1A25C]", active: false },
 ];
 
 const update = () => {
@@ -51,7 +51,7 @@ function ColorPalette({ title, children, className, ...props }: Props) {
             key={elment.id}
             className={`flex justify-center items-center cursor-pointer ${
               elment.active ? "w-[40px] h-[40px]" : "w-[20px] h-[20px]"
-            } rounded-full bg-[${elment.colorHEX}] ml-[14px] transition-all duration-500 ease-in-out`}
+            } rounded-full ${elment.colorHEX} ml-[14px] transition-all duration-500 ease-in-out`}
             onClick={() => {
               update();
               active(elment.id);

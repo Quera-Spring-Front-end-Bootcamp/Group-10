b3d9667c32bd89a21374309f910b8eb52047b6a6
SeyedAmirMohammad <41320511+SeyedAmirMohammad@users.noreply.github.com>
2023-05-25 16:45:34 +0330

    FileInput design done 12th commit Amirmuhammad on FileInput commit

--- src/components/ui/FileInput.tsx
+++ src/components/ui/FileInput.tsx
@@ -1,19 +1,44 @@
-import React, { PropsWithChildren } from "react";
+import React, { useRef, useEffect, PropsWithChildren, useState } from "react";
 import Button from "./Button";
 
 interface Props
   extends PropsWithChildren<React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>> {
-  Src?: any;
+  bg_text?: string;
+  title?: string;
+  desc?: string;
   action?: () => void;
 }
 
-function FileInput({ Src, action, children, className, ...props }: Props) {
+function FileInput({
+  bg_text,
+  title = "ویرایش تصویر فایل",
+  desc = "این تصویر برای عموم قابل رویت است",
+  action,
+  children,
+  className,
+  ...props
+}: Props) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [img, setImg]: any = useState([]);
+
+  const inputAction = () => {
+    inputRef.current?.click();
+    setImg([inputRef.current?.files]);
+  };
+
   return (
-    <div className="">
-      <div className=""></div>
+    <div className="w-fit flex justify-center items-center gap-[20px] ">
+      <div className="w-[100px] h-[100px] flex justify-center items-center rounded-full text-[32px] bg-[#EAF562]  font-[500]">
+        {bg_text}
+      </div>
       <div className="flex flex-col">
-        <input type="file" className="before:content-['_↗']"/>
-        <span className="text-[12px] text-right text-[#8A8989]">این تصویر برای عموم قابل رویت است</span>
+        <Button className="flex justify-center items-center w-[185px] h-[40px] bg-white text-primary border border-primary mb-4">
+          <input type="file" className="w-[185px] h-[40px] hidden max-w-full" ref={inputRef} />
+          <p onClick={inputAction} className="text-primary  z-10">
+            {title}
+          </p>
+        </Button>
+        <span className="text-[12px] text-right text-[#8A8989] w-fit"> {desc} </span>
       </div>
     </div>
   );

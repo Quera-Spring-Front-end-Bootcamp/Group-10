1eeb742389a632e937512fe927a303de04f1705c
Dani <danial@Danis-MacBook-Pro.local>
2023-05-25 17:32:54 +0330

    fix component and add props

--- src/components/ui/Timer.tsx
+++ src/components/ui/Timer.tsx
@@ -1,18 +1,33 @@
-import React, { useState, useEffect } from "react";
-import PlayIconSVG from "../icons/playIcon";
-import StopIconSVG from "../icons/StopIcon";
+import { useState, useEffect } from "react";
+import PlayIcon from "../icons/PlayIcon";
+import PauseIcon from "../icons/PauseIcon";
+import IconButton from "./IconButton";
 
-const Timer: React.FC = () => {
-  const [seconds, setSeconds] = useState<number>(0);
-  const [minutes, setMinutes] = useState<number>(0);
-  const [hours, setHours] = useState<number>(0);
+type Props = {
+  second?: number;
+  minute?: number;
+  hour?: number;
+  onPause?: ({
+    seconds,
+    minutes,
+    hours,
+  }: {
+    seconds: number;
+    minutes: number;
+    hours: number;
+  }) => void;
+};
+
+function Timer({ second = 0, minute = 0, hour = 0, onPause }: Props) {
+  const [seconds, setSeconds] = useState<number>(second);
+  const [minutes, setMinutes] = useState<number>(minute);
+  const [hours, setHours] = useState<number>(hour);
   const [isActive, setIsActive] = useState<boolean>(false);
 
   useEffect(() => {
     let intervalId: number;
-
     if (isActive) {
-      intervalId = window.setInterval(() => {
+      intervalId = setInterval(() => {
         if (seconds === 59) {
           setSeconds(0);
           if (minutes === 59) {
@@ -25,33 +40,35 @@ const Timer: React.FC = () => {
           setSeconds(seconds + 1);
         }
       }, 1000);
+    } else if (onPause) {
+      onPause({
+        seconds,
+        minutes,
+        hours,
+      });
     }
 
-    return () => window.clearInterval(intervalId);
-  }, [isActive, seconds, minutes, hours]);
+    return () => clearInterval(intervalId);
+  }, [isActive, seconds, minutes, hours, onPause]);
 
-  const handleStartStopClick = () => setIsActive(!isActive);
+  function handleStartStopClick() {
+    setIsActive(!isActive);
+  }
 
   return (
-    <div className="flex flex-col bg-white p-2 relative">
-      <p className="text-xs text-gray pb-2">زمان</p>
-      <div className="flex flex-row">
-        <button
-          className="absolute bottom-3 right-1"
-          onClick={handleStartStopClick}
-        >
-          {isActive ? (
-            <PlayIconSVG className="text-white bg-greenSvg rounded-full" />
-          ) : (
-            <StopIconSVG className="text-white bg-greenSvg rounded-full p-1" />
-          )}
-        </button>
-        <h2 className="mr-6">{`${hours.toString().padStart(2, "0")}:${minutes
-          .toString()
-          .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`}</h2>
-      </div>
+    <div className="flex flex-row bg-white gap-2 items-center">
+      <IconButton onClick={handleStartStopClick} className="p-0.5">
+        {isActive ? (
+          <PauseIcon className="stroke-white bg-red-400 rounded-full w-5 h-5" />
+        ) : (
+          <PlayIcon className="stroke-white bg-green-400 rounded-full w-5 h-5" />
+        )}
+      </IconButton>
+      <span>{`${hours < 10 ? "0" : ""}${hours}:${
+        minutes < 10 ? "0" : ""
+      }${minutes}:${seconds < 10 ? "0" : ""}${seconds}`}</span>
     </div>
   );
-};
+}
 
 export default Timer;

bd72bff2044dcf2b2512ef9611395be31002be6f
Dani <danial@Danis-MacBook-Pro.local>
2023-05-26 01:24:57 +0330

    fix modal

--- src/pages/UI.tsx
+++ src/pages/UI.tsx
@@ -1,11 +1,9 @@
+import Card from "../components/ui/Card";
 import FileInput from "../components/ui/FileInput";
+import Modal from "../components/ui/Modal";
 
 function UI() {
-  return (
-    <div>
-      <FileInput />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default UI;

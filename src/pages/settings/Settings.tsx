fa30eb89938f20a3fc587203973dfb8d28c3c24b
Dani <danial@Danis-MacBook-Pro.local>
2023-05-27 02:01:02 +0330

    white space

--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import TicIcon from "../../components/icons/TicIcon";
+
 import Button from "../../components/ui/Button";
 import ColorPalette from "../../components/ui/ColorPalette";
 import SwitchButton from "../../components/ui/SwitchButton";

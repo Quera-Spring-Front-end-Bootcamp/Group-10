8fd376665b5769856ec51fbc9e7f0ea62492b9f1
mostafa <mostafa.shahnani6898@gmail.com>
2023-05-26 18:47:10 +0330

    account page edite

--- src/pages/UI.tsx
+++ src/pages/UI.tsx
@@ -1,9 +1,5 @@
-import Card from "../components/ui/Card";
-import FileInput from "../components/ui/FileInput";
-import Modal from "../components/ui/Modal";
-
 function UI() {
-  return <div></div>;
+  return <div>ui</div>;
 }
 
 export default UI;
--- src/pages/settings/Account.tsx
+++ src/pages/settings/Account.tsx
@@ -1,7 +1,73 @@
-import React from "react";
+import { useForm } from "react-hook-form";
+import TextInput from "../../components/ui/TextInput";
+import Button from "../../components/ui/Button";
+
+type RegisterFormData = {
+  email: string;
+  username: string;
+  password: number;
+  isAccepted: boolean;
+};
 
 function Account() {
-  return <div>Account</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormData>({
+    mode: "onTouched",
+  });
+
+  function handleSubmitForm(data: RegisterFormData) {
+    console.log(data);
+  }
+
+  return (
+    <div className="flex flex-col items-start justify-start pt-40 pr-14">
+      <h1 className="font-bold text-lg sm:text-xl md:text-2xl lg:text-3xl pb-8">
+        اطلاعات حساب
+      </h1>
+      <form onSubmit={handleSubmit(handleSubmitForm)} className="mt-7 w-80">
+        <TextInput
+          type="email"
+          label="ایمیل"
+          className="w-full"
+          register={register("email", {
+            required: "این فیلد الزامی است!",
+          })}
+          name="email"
+          hint={errors.email?.message}
+          containerClassName="mb-4"
+        />
+        <TextInput
+          type="password"
+          label="رمز عبور"
+          className="w-full"
+          register={register("password", {
+            min: 8,
+            required: "این فیلد الزامی است!",
+          })}
+          name="password"
+          hint={errors.password?.message}
+          containerClassName="mb-4"
+        />
+        <TextInput
+          type="text"
+          label="نام کاربری"
+          className="w-full"
+          register={register("username", {
+            required: "این فیلد الزامی است!",
+          })}
+          name="username"
+          hint={errors.username?.message}
+          containerClassName="mb-4"
+        />
+        <Button className="w-full mt-8" type="submit">
+          ثبت تغییرات
+        </Button>
+      </form>
+    </div>
+  );
 }
 
 export default Account;

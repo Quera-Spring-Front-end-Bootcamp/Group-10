28f3db72a0141ce14efb082c44af809a2245a301
Dani <danial@Danis-MacBook-Pro.local>
2023-05-27 02:15:17 +0330

    fix padding top for pages and validation in mobile field

--- src/pages/settings/Personal.tsx
+++ src/pages/settings/Personal.tsx
@@ -1,13 +1,13 @@
 import { useForm } from "react-hook-form";
+
 import FileInput from "../../components/ui/FileInput";
 import TextInput from "../../components/ui/TextInput";
 import Button from "../../components/ui/Button";
 
 type RegisterFormData = {
   name: string;
-  lastname: string;
-  tell: number;
-  isAccepted: boolean;
+  lastName: string;
+  mobile: number;
 };
 
 function Personal() {
@@ -24,7 +24,7 @@ function Personal() {
   }
 
   return (
-    <div className="flex flex-col items-start justify-start pt-10 pr-12">
+    <div className="flex flex-col items-start justify-start pt-24 pr-14">
       <h1 className="font-bold text-lg sm:text-xl md:text-2xl lg:text-3xl pb-4">
         اطلاعات فردی
       </h1>
@@ -45,23 +45,34 @@ function Personal() {
           type="text"
           label="نام خانوادگی"
           className="w-full"
-          register={register("lastname", {
+          register={register("lastName", {
             required: "این فیلد الزامی است!",
           })}
-          name="lastname"
-          hint={errors.lastname?.message}
+          name="lastName"
+          hint={errors.lastName?.message}
           containerClassName="mb-4"
         />
         <TextInput
           type="tell"
           label="شماره موبایل"
           className="w-full"
-          register={register("tell", {
-            max: 11,
+          register={register("mobile", {
+            maxLength: {
+              value: 11,
+              message: "شماره همراه باید ۱۱ رقم باشد!",
+            },
+            minLength: {
+              value: 11,
+              message: "شماره همراه باید ۱۱ رقم باشد!",
+            },
+            pattern: {
+              value: /^\d+$/,
+              message: "در این فیلد باید مقادیر عددی وارد شود",
+            },
             required: "این فیلد الزامی است!",
           })}
-          name="tell"
-          hint={errors.tell?.message}
+          name="mobile"
+          hint={errors.mobile?.message}
           containerClassName="mb-4"
         />
         <Button className="w-full mt-8" type="submit">

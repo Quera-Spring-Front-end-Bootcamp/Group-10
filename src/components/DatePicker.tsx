b297507c0972347092b17169ec545639d4a45bea
Dani <danial@Danis-MacBook-Pro.local>
2023-06-03 21:02:09 +0330

    some changes

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,14 +1,38 @@
 import { Calendar } from "react-multi-date-picker";
 import persian from "react-date-object/calendars/persian";
 import persian_fa from "react-date-object/locales/persian_fa";
-import Card from "./ui/Card";
+import DateObject from "react-date-object";
 
 export default function DatePicker() {
+  function MyPlugin({ state, setState, calendar, locale, format }: any) {
+    function handleAddDate(numberOfDaysFromNow: number) {
+      const aa = new DateObject({ calendar, locale, format });
+      console.log([...state.selectedDate, aa]);
+
+      // const date = new Date();
+      // date.setDate(date.getDate() + numberOfDaysFromNow);
+      setState({
+        ...state,
+        selectedDate: [...state.selectedDate, aa],
+      });
+    }
+    return (
+      <div className="w-[200px]">
+        <button
+          className="flex w-full items-center justify-end"
+          onClick={() => handleAddDate(0)}
+        >
+          امروز
+        </button>
+      </div>
+    );
+  }
   return (
     <Calendar
       calendar={persian}
       locale={persian_fa}
-      className="w-[500px] [&.rmdp-calendar]:w-full"
+      multiple
+      plugins={[<MyPlugin />]}
     />
   );
 }
--- src/pages/UI.tsx
+++ src/pages/UI.tsx
@@ -1,16 +1,9 @@
-import { useState } from "react";
 import DatePicker from "../components/DatePicker";
 
 function UI() {
-  const [value, setValue] = useState<string>("");
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setValue(e.target.value);
-    // console.log(11);
-  }
   return (
     <div className="bg-red-200 v-screen h-screen">
       <DatePicker />
-      <input onChange={handleChange} value={value} />
     </div>
   );
 }

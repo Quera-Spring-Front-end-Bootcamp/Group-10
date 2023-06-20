9049e01c8bb103e2742dd0ebb612238c7259756b
Dani <danial@Danis-MacBook-Pro.local>
2023-06-20 18:47:09 +0330

    fix params

--- src/api/User/GetUserByUserNameOrId.ts
+++ src/api/User/GetUserByUserNameOrId.ts
@@ -1,28 +1,29 @@
 import { useQuery } from "@tanstack/react-query";
-import { ResponseBody } from "../Types"
+import { ResponseBody } from "../Types";
 import AXIOS from "../../utils/axios";
 
-
 type UserGetUserByUserNameOrIdResponse = ResponseBody<{
-    "_id": string,
-    "username": string,
-    "firstname": string,
-    "lastname": string,
-    "email": string,
-    "profile_url": string,
-    "phone": string,
-    "workspaces": string[],
-    "workspaceMember": [],
-    "taskAssignees": [],
-    "comments": [],
-    "projectMember": string[]
-}>
+  _id: string;
+  username: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  profile_url: string;
+  phone: string;
+  workspaces: string[];
+  workspaceMember: [];
+  taskAssignees: [];
+  comments: [];
+  projectMember: string[];
+}>;
 
-
-export function GetUserByUserNameOrId() {
-    async function getUserByUserNameOrId(): Promise<UserGetUserByUserNameOrIdResponse> {
-      const { data } = await AXIOS.get("/users/:usernameOrId");
-      return data;
-    }
-    return useQuery(["getUserByUserNameOrId"], getUserByUserNameOrId);
+export function UserGetUserByUserNameOrId(usernameOrId: string) {
+  async function userGetUserByUserNameOrId(): Promise<UserGetUserByUserNameOrIdResponse> {
+    const { data } = await AXIOS.get(`/users/${usernameOrId}`);
+    return data;
   }
+  return useQuery(
+    ["getUserByUserNameOrId", usernameOrId],
+    userGetUserByUserNameOrId
+  );
+}

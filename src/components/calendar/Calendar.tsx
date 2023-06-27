8e35cac4112c173ccf83e9b590f3e9a0fbfb9ea4
Dani <danial@Danis-MacBook-Pro.local>
2023-06-27 19:41:24 +0330

    add flag icon

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -4,7 +4,7 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
 import faLocale from "@fullcalendar/core/locales/fa";
 import listPlugin from "@fullcalendar/list";
-import { Dialog, Transition } from "@headlessui/react";
+import { Dialog, Popover, Transition } from "@headlessui/react";
 import PlusIcon from "../icons/PlusIcon";
 import CloseIcon from "../icons/CloseIcon";
 import { useMemo, useState } from "react";
@@ -12,6 +12,7 @@ import Button from "../ui/Button";
 import { DayCellContentArg } from "@fullcalendar/core/index.js";
 import { toJalaliDate } from "../../helpers/toJalaliDate";
 import "./style.css";
+import FlagIcon from "../icons/FlagIcon";
 
 function Calendar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -48,38 +49,50 @@ function Calendar() {
 
           <div className="fixed inset-0 overflow-y-auto">
             <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel className="w-[500px] max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all border-primary border">
-                  <div className="mt-2 flex flex-row">
-                    <button>
-                      <CloseIcon
-                        className="stroke-gray w-10 h-10"
-                        onClick={closeModal}
-                      />
-                    </button>
-                    <input
-                      placeholder="نام تسک را وارد کنید"
-                      className="flex-1"
+              <Dialog.Panel className="fixed w-[500px] max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all border-primary border">
+                <div className="mt-2 flex flex-row">
+                  <button>
+                    <CloseIcon
+                      className="stroke-gray w-10 h-10"
+                      onClick={closeModal}
                     />
-                  </div>
+                  </button>
+                  <input
+                    placeholder="نام تسک را وارد کنید"
+                    className="flex-1"
+                  />
+                </div>
 
-                  <div className="mt-4 flex flex-row justify-between items-center">
-                    <div>
-                      <p>{ShowDate}</p>
-                    </div>
-                    <Button type="button" onClick={closeModal}>
-                      ساختن تسک
-                    </Button>
+                <div className="mt-4 flex flex-row justify-between items-center">
+                  <div className="flex">
+                    <button>
+                      <FlagIcon className="stroke-red-400" />
+                    </button>
+                    <p>{ShowDate}</p>
                   </div>
-                </Dialog.Panel>
-              </Transition.Child>
+                  <Button type="button" onClick={closeModal}>
+                    ساختن تسک
+                  </Button>
+                </div>
+              </Dialog.Panel>
+              <div className="fixed z-50 bg-white overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                <div className="relative grid gap-8 bg-white p-7 lg:grid-cols-2"></div>
+                <div className="bg-gray-50 p-4">
+                  <a
+                    href="##"
+                    className="flow-root rounded-md px-2 py-2 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
+                  >
+                    <span className="flex items-center">
+                      <span className="text-sm font-medium text-gray-900">
+                        Documentation
+                      </span>
+                    </span>
+                    <span className="block text-sm text-gray-500">
+                      Start integrating products and tools
+                    </span>
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </Dialog>
